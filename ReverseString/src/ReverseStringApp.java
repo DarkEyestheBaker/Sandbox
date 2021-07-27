f41408c21b1704415017db88975157f31339af05
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-07-26 21:23:22 -0400

    Reverse String

--- ReverseString/src/ReverseStringApp.java
+++ ReverseString/src/ReverseStringApp.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 public class ReverseStringApp {
 
 	public static void main(String[] args) {
 
-		String str = "are, you, as, bored, as, I, am";
-		System.out.println(str);
-		String a[] = str.split(",");
+		public static void main(String[] args) {
 
-		for (int i = a.length - 1; i >= 0; i--) {
-			System.out.print(a[i] + ", ");
+			System.out.println("Enter words to be reversed here:   ");
+
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
+			String a[] = input.split(" ");
+
+			System.out.print("Reversed string =  ");
+			for (int i = a.length - 1; i >= 0; i--) {
+				System.out.print(a[i] + ", ");
+			}
 		}
-
 	}
-
-}
