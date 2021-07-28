5fd387e8012248827cd0f3576abba4659b693041
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-07-28 10:12:30 -0400

    Hello, World Abre!

--- ParseTextAndCount/src/ParseTextAndCountApp.java
+++ ParseTextAndCount/src/ParseTextAndCountApp.java
@@ -3,28 +3,27 @@ import java.io.*;
 
 public class ParseTextAndCountApp {
 
-		public static void main(String[] args) throws FileNotFoundException {
-
-			HashMap<String, Integer> map = new HashMap<String, Integer>();
-			
-				Scanner txtfile = new Scanner(new File("C:\\repos-08-2020\\sandbox\\TextToParse.txt"));
-				while (txtfile.hasNext()) {
-					
-					String word = txtfile.next();
-					Integer count = map.get(word);
-
-					if (count != null) {
-						count++;
-					} else {
-						count = 1;
-					}
-				}
-				txtfile.close();
-				for (Map.Entry<String, Integer> entry : map.entrySet()) {
-					System.out.println(entry);
-				}
+		
+			public void CountWords(String filename, Map< String, Integer> words) throws FileNotFoundException
+			{
+			Scanner file=new Scanner (new File(filename));
+			while(file.hasNext()){
+			String word=file.next();
+			Integer count=words.get(word);
+			if(count!=null)
+			count++;
+			else
+			count=1;
+			words.put(word,count);
 			}
-
+			file.close();
+			}
+			public static void main(String[] args)
+			{
+			Map<String,Integer> words=new HashMap<String, Integer>();
+			CountWords("transcript.txt",words);
+			System.out.println(words);
+			}
 			}
 			
 
